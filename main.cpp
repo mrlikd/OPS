1d1f34378359731492a86dcc5c648c2cb7ac7513
mrlikd <david.mrlik@tul.cz>
2023-10-17 11:04:33 +0000

    Fixed bug not blinking, deleted functions for test

--- main.cpp
+++ main.cpp
@@ -1,14 +1,11 @@
 #include <string>
-#include "stdio.h"
+#include <chrono>
 #include "mbed.h"
-#include <iostream>
 
 #define COMMA_PAUSE_LENGTH 1000ms
 #define DOT_PAUSE_LENGTH 500ms
 
 DigitalOut led(LED1);
-/*DigitalOut led(LED1);*/
-using namespace std;
 
 void blink(string symb){
     for (int i = 0; i < sizeof(symb); i++) {
@@ -17,12 +14,14 @@ void blink(string symb){
                 led = 1;
                 ThisThread::sleep_for(DOT_PAUSE_LENGTH);
                 led = 0;
+                ThisThread::sleep_for(DOT_PAUSE_LENGTH);
                 //cout << ".";
                 break;
             case '-': 
                 led = 1;
                 ThisThread::sleep_for(COMMA_PAUSE_LENGTH);      
                 led = 0;
+                ThisThread::sleep_for(COMMA_PAUSE_LENGTH);
                 //cout << "-";
                 break;
         }                    
@@ -36,7 +35,7 @@ void blink(string symb){
 int main()
 {
     
-    string word("david");
+    string word("DAVID");
     string A(".-");
     string B("-...");
     string C("-.-.");
@@ -64,28 +63,113 @@ int main()
     string Y("-.--");
     string Z("--..");
     
-    int test = 0;
-    while (true && test < 1) {
+    while (true) {
         for (int i = 0; i < sizeof(word); i++) {
             switch(word[i]){
-                case 'a':
-                    cout<< 'A';
-                    blink(A);
-                    break;
-                case 'd':    
-                    cout<< 'D';
-                    blink(D);
-                    break;
-                case 'i':    
-                    cout<< 'I';
-                    blink(I);
-                    break;
-                case 'v':    
-                    cout << 'V';
-                    blink(V);
-                    break;
+            case 'A':
+                blink(A);
+                break;
+            case 'B':
+                blink(B);
+                break;
+            case 'C':
+                
+                blink(C);
+                break;
+            case 'D':
+                
+                blink(D);
+                break;
+            case 'E':
+                
+                blink(E);
+                break;
+            case 'F':
+                
+                blink(F);
+                break;
+            case 'G':
+                
+                blink(G);
+                break;
+            case 'H':
+                
+                blink(H);
+                break;
+            case 'I':
+                
+                blink(I);
+                break;
+            case 'J':
+                
+                blink(J);
+                break;
+            case 'K':
+                
+                blink(K);
+                break;
+            case 'L':
+                
+                blink(L);
+                break;
+            case 'M':
+                
+                blink(M);
+                break;
+            case 'N':
+               
+                blink(N);
+                break;
+            case 'O':
+                
+                blink(O);
+                break;
+            case 'P':
+                
+                blink(P);
+                break;
+            case 'Q':
+                
+                blink(Q);
+                break;
+            case 'R':
+                
+                blink(R);
+                break;
+            case 'S':
+                
+                blink(S);
+                break;
+            case 'T':
+                
+                blink(T);
+                break;
+            case 'U':
+                
+                blink(U);
+                break;
+            case 'V':
+                
+                blink(V);
+                break;
+            case 'W':
+                
+                blink(W);
+                break;
+            case 'X':
+                
+                blink(X);
+                break;
+            case 'Y':
+                
+                blink(Y);
+                break;
+            case 'Z':
+                
+                blink(Z);
+                break;
             }   
         }
-        test++;
+        
     }
 } 
