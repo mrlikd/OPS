18be663a76c43647601a924b4bcfdc0bbb68d025
mrlikd <david.mrlik@tul.cz>
2023-10-24 10:51:16 +0000

    Rozcleneni do hlavicek

---
+++ Morse.h
@@ -0,0 +1,12 @@
+#include <string>
+#include "mbed.h"
+#include <chrono>
+
+#define COMMA_PAUSE_LENGTH 1000ms
+#define DOT_PAUSE_LENGTH 500ms
+
+
+void playMorse(string word);
+void blink(string letter);
+
+
